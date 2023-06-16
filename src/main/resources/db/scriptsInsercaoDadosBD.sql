17f7ace53f7c5a669dcaca7107e8668633c69d2a
OPaje <jecssilva@gmail.com>
2023-06-16 11:41:29 -0300

    script para inserção de dados no banco

---
+++ src/main/resources/db/scriptsInsercaoDadosBD.sql
@@ -0,0 +1,13 @@
+INSERT INTO plataforma (codigo, nome) VALUES
+(1, 'PS4'),
+(2, 'PS5'),
+(3, 'XBOX ONE'),
+(4, 'NINTENDO SWITCH'),
+(5, 'XBOX SERIES X'),
+(6, 'PC');
+
+
+
+INSERT INTO papel (codigo, nome) VALUES 
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USUARIO');
